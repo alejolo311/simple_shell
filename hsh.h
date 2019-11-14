c16819feb7513f33ebc6e9426a8a2cc853139c48
alejolo311 <1147@holbertonschool.com>
2019-11-14 12:54:39 +0000

    create a basic main, create the basic structure of the header file and add the posible libraries to use

---
+++ hsh.h
@@ -0,0 +1,13 @@
+#ifndef H_SHELL
+#define H_SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <syscalls.h>
+
+
+#endif /* H_SHELL */
