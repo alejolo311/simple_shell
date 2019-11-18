c626bcd91227a47b0f16ca8b1a0b8ba214a3b282
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-18 17:29:56 +0000

    Taking into account parameters for commads

--- test_g/interact.c
+++ test_g/interact.c
@@ -12,16 +12,17 @@ int interact(char **av, char **env)
 {
 size_t len = 0;
 int read = 1, j, argc, inter = 1;
-char *str1, *token, *saveptr1, **argv, *line = NULL;
+char *str1, *token, **argv, *line = NULL, *tmp = NULL;
 
 	isatty(STDIN_FILENO) == 0 ? inter = 0 : inter;
 	inter == 1 ?  printf("#cisfun$ ") : inter;
 	read = getline(&line, &len, stdin);
 	while (read)
 	{
-		for (argc = 1, str1 = line; ; argc++, str1 = NULL)
+		tmp = strdup(line);
+		for (argc = 1, str1 = tmp; ; argc++, str1 = NULL)
 		{
-			token = strtok_r(str1, " \t\n", &saveptr1);
+			token = strtok(str1, " \t\n");
 			if (token == NULL)
 				break;
 		}
@@ -35,18 +36,17 @@ char *str1, *token, *saveptr1, **argv, *line = NULL;
 		argv[0] = av[0];
 		for (j = 1, str1 = line; ; j++, str1 = NULL)
 		{
-			token = strtok_r(str1, " \t\n", &saveptr1);
+			token = strtok(str1, " \t\n");
 			argv[j] = token;
-			printf("[%s]", argv[j]);
 			if (token == NULL)
 				break;
 		}
-		printf("\n");
 		if (argc > 2)
 			myexec(j, argv, env);
 		if (inter == 1)
 			printf("#cisfun$ ");
 		free(argv);
+		free(tmp);
 		read = getline(&line, &len, stdin);
 		if (strncmp(line, "exit", 4) == 0 || read == -1)
 		{
