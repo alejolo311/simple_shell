d7d1de4ca36e54873d82d005994e26a47a658ce8
alejolo311 <1147@holbertonschool.com>
2019-11-24 18:22:46 -0500

    preliminry versions of set and unset

--- _setenv.c
+++ _setenv.c
@@ -9,10 +9,34 @@
  */
 int _setenv(char **argv, char **env, unsigned int *execnt)
 {
-	(void) argv;
-	(void) env;
+
+	int i, j;
+	char *var, *value, *_value;
 	(void) execnt;
 
-	printf("setenv\n");
+  	var = argv[2];
+  	value = argv[3];
+  	if (var && value)
+    	{
+     		for(i = 0; env[i] != NULL; i++)
+		{
+			if (strncmp(env[i], var, strlen(var)) == 0)
+			{
+				break;
+			}
+		}
+		_value = env[i] + (strlen(var) + 1);
+
+		for (j = 0; j < strlen(value); j++)
+		{
+			_value[j] = value[j];
+		}
+		_value[j] = '\0';
+	}
+	else
+		write(STDERR_FILENO,"Usage: setenv VARIABLE VALUE\n", 29);
+
 	return (1);
+
 }
+
