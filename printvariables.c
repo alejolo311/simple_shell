dee03ac2cb2568f400a3108bd79ff0287656ce16
alejolo311 <1147@holbertonschool.com>
2019-11-24 18:23:17 -0500

    dummies of alias and variables

---
+++ printvariables.c
@@ -0,0 +1,36 @@
+#include "hsh.h"
+/**
+ * _setenv - function setenv
+ * desc: set a enviroment variable
+ * @argv: the parameters
+ * @execnt: the counter
+ * @env: the enviroment
+ * Return: 1 in success.
+ */
+int printvariable(char **argv, char **env, unsigned int *execnt)
+{
+
+	int i, j;
+	char *var, *value, *_value;
+	(void) execnt;
+
+  	var = argv[1] + 1;
+  	if (var)
+    	{
+     		for(i = 0; env[i] != NULL; i++)
+		{
+			if (strncmp(env[i], var, strlen(var)) == 0)
+			{
+				break;
+			}
+		}
+		_value = env[i] + (strlen(var) + 1);
+		printf("%s", _value);
+	}
+	else
+		write(STDERR_FILENO,"Usage:$VARIABLE\n", 15);
+
+	return (1);
+
+}
+
