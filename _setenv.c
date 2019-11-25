0e7b220254db7b376ff1df8dc2ebbd85c2641934
alejolo311 <1147@holbertonschool.com>
2019-11-25 21:39:25 +0000

    seconde version of setenv

--- _setenv.c
+++ _setenv.c
@@ -7,31 +7,30 @@
  * @env: the enviroment
  * Return: 1 in success.
  */
-int _setenv(char **argv, char **env, unsigned int *execnt)
+int _setenv(char **argv, lenv_s **lenv, unsigned int *execnt)
 {
 
 	int i, j;
-	char *var, *value, *_value;
+	char *variable, *value, *_value, *var;
 	(void) execnt;
+	lenv_s *h = *lenv;
+	char *newvalue;
 
-  	var = argv[2];
+  	variable = argv[2];
   	value = argv[3];
-  	if (var && value)
+  	if (variable && value)
     	{
-     		for(i = 0; env[i] != NULL; i++)
+     		for(i = 0; h; i++, h = h->next)
 		{
-			if (strncmp(env[i], var, strlen(var)) == 0)
+			var = h->var;
+			if (strncmp(var, variable, strlen(variable)) == 0)
 			{
+				free(h->var);
+				newvalue = strdup("sebas");
+				h->var = newvalue;
 				break;
 			}
 		}
-		_value = env[i] + (strlen(var) + 1);
-
-		for (j = 0; j < strlen(value); j++)
-		{
-			_value[j] = value[j];
-		}
-		_value[j] = '\0';
 	}
 	else
 		write(STDERR_FILENO,"Usage: setenv VARIABLE VALUE\n", 29);
